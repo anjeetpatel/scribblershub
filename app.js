f8022c0fb8d778a430829efc5b1c9794c025b160
Anjeet <anjeetpatel0306@gmail.com>
2024-04-20 21:59:19 +0530

    database connection

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
 const app = express();
 const PORT = 5000 || process_env.PORT;
+
+//connect to db 
+connectDB();
 
 app.use(express.static("public"));
 
@@ -16,5 +21,5 @@ app.set("view engine", "ejs");
 app.use("/", require("./server/routes/main"));
 
 app.listen(PORT, () => {
-  console.log(`app listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
