4dfa17088bafe15da66b5540b0ef22615f438032
Anjeet <anjeetpatel0306@gmail.com>
2024-04-21 14:10:01 +0530

    Auth completed

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
-
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
 const connectDB = require("./server/config/db");
+const session = require("express-session");
 
 const app = express();
 const PORT = 5000 || process_env.PORT;
@@ -13,6 +15,18 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MongoDB_URI,
+    }),
+  })
+);
 
 app.use(express.static("public"));
 
@@ -22,6 +36,7 @@ app.set("layout", "./layout/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
