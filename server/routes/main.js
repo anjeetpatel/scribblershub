bf31fe509de1615a5000dd83f857dd1aead1ee4a
Anjeet <anjeetpatel0306@gmail.com>
2024-04-21 22:22:29 +0530

    final commit

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../model/Post");
-//routes
 
+/**
+ * GET /
+ * HOME
+ */
 router.get("", async (req, res) => {
   try {
     const locals = {
-      title: "NodeJs blog",
-      description: "NodeJs in MongoDB",
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
     };
 
     let perPage = 10;
@@ -18,7 +21,7 @@ router.get("", async (req, res) => {
       .limit(perPage)
       .exec();
 
-    const count = await Post.countDocuments();
+    const count = await Post.countDocuments({});
     const nextPage = parseInt(page) + 1;
     const hasNextPage = nextPage <= Math.ceil(count / perPage);
 
@@ -27,78 +30,17 @@ router.get("", async (req, res) => {
       data,
       current: page,
       nextPage: hasNextPage ? nextPage : null,
+      currentRoute: "/",
     });
-
-    res.render("index", { locals, data });
   } catch (error) {
     console.log(error);
   }
 });
 
-// router.get("", async (req, res) => {
-//   const locals = {
-//     title: "NodeJs blog",
-//     description: "nodejs in mongo db",
-//   };
-
-//   try {
-//     const data = await Post.find();
-//     res.render("index", { locals, data });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// function insertPostData() {
-//   Post.insertMany([
-//     {
-//       title: "Building a Blog",
-//       body: "This is the body text",
-//     },
-//     {
-//       title: "Building APIs with Node.js",
-//       body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js",
-//     },
-//     {
-//       title: "Deployment of Node.js applications",
-//       body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments...",
-//     },
-//     {
-//       title: "Authentication and Authorization in Node.js",
-//       body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries.",
-//     },
-//     {
-//       title: "Understand how to work with MongoDB and Mongoose",
-//       body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications.",
-//     },
-//     {
-//       title: "build real-time, event-driven applications in Node.js",
-//       body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js.",
-//     },
-//     {
-//       title: "Discover how to use Express.js",
-//       body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications.",
-//     },
-//     {
-//       title: "Asynchronous Programming with Node.js",
-//       body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations.",
-//     },
-//     {
-//       title: "Learn the basics of Node.js and its architecture",
-//       body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers.",
-//     },
-//     {
-//       title: "NodeJs Limiting Network Traffic",
-//       body: "Learn how to limit netowrk traffic.",
-//     },
-//     {
-//       title: "Learn Morgan - HTTP Request logger for NodeJs",
-//       body: "Learn Morgan.",
-//     },
-//   ]);
-// }
-// insertPostData();
-
+/**
+ * GET /
+ * Post :id
+ */
 router.get("/post/:id", async (req, res) => {
   try {
     let slug = req.params.id;
@@ -107,24 +49,32 @@ router.get("/post/:id", async (req, res) => {
 
     const locals = {
       title: data.title,
-      description: "NodeJs in MongoDB.",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
     };
 
-    res.render("post", { locals, data });
+    res.render("post", {
+      locals,
+      data,
+      currentRoute: `/post/${slug}`,
+    });
   } catch (error) {
     console.log(error);
   }
 });
 
+/**
+ * POST /
+ * Post - searchTerm
+ */
 router.post("/search", async (req, res) => {
   try {
     const locals = {
-      title: "Search",
-      description: "nodejs in mongo db",
+      title: "Seach",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
     };
 
     let searchTerm = req.body.searchTerm;
-    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
 
     const data = await Post.find({
       $or: [
@@ -133,17 +83,24 @@ router.post("/search", async (req, res) => {
       ],
     });
 
-    res.render("search",{
+    res.render("search", {
       data,
-      locals
+      locals,
+      currentRoute: "/",
     });
   } catch (error) {
     console.log(error);
   }
 });
 
+/**
+ * GET /
+ * About
+ */
 router.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {
+    currentRoute: "/about",
+  });
 });
 
 module.exports = router;
